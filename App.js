26e5cb08a9d6dc4f5e565ff3a47b48c2a1e6ae1c
Gianluca Bragaglia <gianluca@Gianlucas-Mac-mini.local>
2018-05-10 14:37:20 +0200

    ok

--- App.js
+++ App.js
@@ -1,86 +1,24 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-/* import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import InsertEmail from './components/InsertEmail';
-
-
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-
-        <View style={styles.firstBlock}>
-          <InsertEmail />
-        </View>
-        <View style={styles.secondBlock}>
-          <Text>TAKE A PHOTO</Text>
-        </View>
-        <View style={styles.firstBlock}>
-          <Text style={styles.textTitol}>SELECT TEXT</Text>
-        </View>
-        <View style={styles.firstBlock}>
-          <Text style={styles.textTitol}>SEND EMAIL !!!!</Text>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    
-  },
-  textTitol: {
-    fontSize: 20,
-    textAlign: 'center',
-    color: '#FFFFFF',
-    marginBottom: 5,
-  },
-  firstBlock: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#67CDDC',
-  },
-  secondBlock: {
-    flex: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#E6E7DF',
-    
-  },
-
-
-});
- */
-
-
 
 
 import React, { Component } from 'react';
-import InsertEmail from './components/InsertEmail';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  TextInput,
+  TouchableHighlight,
+  Picker,
+  Button,
+  Image
+
 } from 'react-native';
 
 // either import the whole module and call as Communications.method()
 import Communications from 'react-native-communications';
+import {Clipboard} from 'react-native';
+
 
 // or can now import single methods and call straight via the method name
 // import { web, phonecall } from 'react-native-communications';
@@ -90,30 +28,65 @@ import Communications from 'react-native-communications';
 
 export default class App extends Component {
 
-  onChangeText = (emailAd) => {
-    const newVal = emailAd;
-    this.setState({emailAd: newVal});
+  constructor(props) {
+    super(props);
+    this.state = {
+     value: '',
+     language: '',
+     
+    }
+    
   }
 
+  setClipboardContent = (msg) => {
+    Clipboard.setString(msg);
+    };
+
   render() {
+
+
     return (
       
       <View style={styles.container}>
 
       <View style={styles.first1Block}>
-        <InsertEmail onChangeText={this.onChangeText} emailAd={this.props.emailAd} />
+      <Image
+          source={require('./img/ic_launcher.png')}
+        />
+      <TextInput 
+            style={{height: 50, width: 300, color: '#6A9736', backgroundColor: 'white', fontSize: 20, borderColor: '#6A9736', borderBottomWidth: 1, marginTop: 10}}
+            value = { this.state.email }
+            underlineColorAndroid={'transparent'}
+            placeholder = 'Insert Email'
+            onChangeText={ text => this.setState({ email: text })}
+            />
       </View>
       <View style={styles.secondBlock}>
-        <Text>TAKE A PHOTO</Text>
+      <Picker
+        selectedValue={this.state.language}
+        style={{ height: 50, width: 200, color: 'white'}}
+        onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
+        <Picker.Item label="1" value="text1" />
+        <Picker.Item label="2" value="text2" />
+        <Picker.Item label="3" value="text3" />
+        <Picker.Item label="4" value="text4" />
+        <Picker.Item label="5" value="text5" />
+        <Picker.Item label="6" value="text6" />
+        <Picker.Item label="7" value="text7" />
+        <Picker.Item label="8" value="text8" />
+      </Picker>
       </View>
       <View style={styles.firstBlock}>
-        <Text style={styles.textTitol}>TEXT</Text>
+      <Button
+          onPress={()=>this.setClipboardContent(this.state.language)} 
+          title="Copy Option"
+        />
       </View>
       <View style={styles.firstBlock}>
         
-        <TouchableOpacity email={this.props.emailAd} onPress={() => Communications.email(['brag@libero.it'],null,null,'My Subject','My body text')}>
+        <TouchableOpacity onPress={() => Communications.email([ this.state.email ],null,null,'My Subject',this.state.language)}>
           <View style={styles.holder}>
-            <Text style={styles.text}>Send an email</Text>
+            <Text style={styles.text}>SEND EMAIL</Text>
           </View>
         </TouchableOpacity>
 
@@ -128,29 +101,31 @@ const styles = StyleSheet.create({
     flex: 1,
     
   },
-  textTitol: {
+  text: {
     fontSize: 20,
     textAlign: 'center',
-    color: '#FFFFFF',
+    color: '#6A9736',
+    borderColor: '#6A9736',
+    borderBottomWidth: 1,
     marginBottom: 5,
   },
   firstBlock: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#67CDDC',
+    backgroundColor: 'white',
   },
   first1Block: {
     flex: 2,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#67CDDC',
+    backgroundColor: 'white',
   },
   secondBlock: {
-    flex: 2,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#E6E7DF',
+    backgroundColor: '#6A9736',
     
   },
 
--- components/InsertEmail.js
+++ components/InsertEmail.js
@@ -1,40 +1,17 @@
-/* import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, View } from 'react-native';
-
-export default class InsertEmail extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { email: '' };
-    
-  }
-
-  
-  
-
-  render() {
-    return (
-      <View>
-        <Text style={{fontSize: 23, color: 'white'}}>Insert Email</Text>
-        <TextInput
-        style={{height: 50, width: 300, color: 'white', fontSize: 23, borderColor: 'white', borderBottomWidth: 1}}
-        onChangeText={(email) => this.setState({email})}
-        value={this.state.email}
-        underlineColorAndroid={'transparent'}
-        />
-      </View>
-    );
-    console.log(this.state.email);
-  }
-} */
 
 
 import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, View } from 'react-native';
+import { AppRegistry, 
+         Text, 
+         TextInput, 
+         View,
+         TouchableHighlight
+} from 'react-native';
+
 
 export default class InsertEmail extends Component {
 
-  constructor(props) {
+/*   constructor(props) {
     super(props);
     this.state = { emailAd: '' };
     
@@ -44,10 +21,37 @@ export default class InsertEmail extends Component {
     const emailAd = 'Bragi'; // somehow calculate new value
     this.props.onChangeText(emailAd),
     this.setState({emailAd})
-  }
+  } */
   
+  constructor() {
+    super();
+    this.state = {
+        email: []
+        //selecText: '1'
+    }
+    this.inputEmail = this.inputEmail.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  render() {
+    inputEmail (e) {
+        const {value, name} = e.target;
+        this.setState({
+        [name]: value        
+        })
+        console.log(e.target.value);
+        
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.onAddEmail(this.state);
+
+  }
+
+
+
+
+  /* render() {
     return (
       <View>
         <Text style={{fontSize: 23, color: 'white'}}>Insert Email</Text>
@@ -60,7 +64,48 @@ export default class InsertEmail extends Component {
       </View>
     );
     console.log(this.state.emailAd);
+  } */
+
+  render() {
+    return (
+      <View>
+        <Text style={{fontSize: 23, color: 'white'}}>Insert Email</Text>
+        
+            <TextInput 
+            style={{height: 50, width: 300, color: 'black', backgroundColor: 'white', fontSize: 23, borderColor: 'white', borderBottomWidth: 1}}
+            name="email" 
+            underlineColorAndroid={'transparent'}
+            onChange={this.inputEmail}
+            />
+            {/* <div className="form-group">
+                <select
+                    name="selecText"
+                    className="form-control"
+                    value={this.state.selecText}
+                    onChange={this.inputEmail}
+                >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                </select>
+           </div> */}
+          <TouchableHighlight style={{marginTop:15, paddingTop: 15, paddingBottom: 15, borderColor: 'white', borderWidth: 1}}
+          onPress={this.handleSubmit}
+          >
+            <Text 
+              
+              style={{textAlign: 'center', color: 'white', fontSize: 20 }}
+            
+            >SAVE</Text>
+          </TouchableHighlight>
+            
+        
+
+      </View>
+    );
   }
+
+
 }
 
 
